d7f6aeb04b15e212acc49286046794ee7e9384c2
juny <junyeongbigdata@naver.com>
2023-08-08 12:18:51 +0900

    Junit5

--- src/test/java/com/example/book/web/BookControllerIntegreTest.java
+++ src/test/java/com/example/book/web/BookControllerIntegreTest.java
@@ -1,10 +1,11 @@
 package com.example.book.web;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 //인테그리 통합테스트 (모든 Bean들을 똑같이 IoC에 올리고 테스트)
 //bean 메모리 저장된 싱글톤방식 저장된 객체들
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK) //실제 톰켓이 아니라 다른 톰캣으로 테스트
 public class BookControllerIntegreTest {
     
 }
