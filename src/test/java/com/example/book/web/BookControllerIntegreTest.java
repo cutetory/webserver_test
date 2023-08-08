f90391b5f517484cb40180486ff4c851aaf89feb
juny <junyeongbigdata@naver.com>
2023-08-08 12:13:34 +0900

    junit5

--- src/test/java/com/example/book/web/BookControllerIntegreTest.java
+++ src/test/java/com/example/book/web/BookControllerIntegreTest.java
@@ -1,7 +1,10 @@
 package com.example.book.web;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
 //인테그리 통합테스트 (모든 Bean들을 똑같이 IoC에 올리고 테스트)
 //bean 메모리 저장된 싱글톤방식 저장된 객체들
+@SpringBootTest
 public class BookControllerIntegreTest {
     
 }
